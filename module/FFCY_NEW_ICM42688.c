2b6f3dd9e0b1ed60eeb77650c8a5c5426c2ae0c0
wty20191019 <906938998@qq.com>
2026-04-24 22:54:43 +0800

    接口对齐 Signed-off-by: wty20191019 <906938998@qq.com>

--- module/FFCY_NEW_ICM42688.c
+++ module/FFCY_NEW_ICM42688.c
@@ -1,26 +1,23 @@
 #include "FFCY_NEW_ICM42688.h"
 #include "i2c1.h"
 
-#define ICM42688_ADDRESS_DEFAULT      0x68
-#define ICM42688_ADDRESS_ALT          0x69
-#define ICM42688_WHO_AM_I            0x75
-#define ICM42688_WHOAMI_VALUE        0x47
-#define ICM42688_REG_BANK_SEL        0x76
-#define ICM42688_PWR_MGMT0           0x4E
-#define ICM42688_ACCEL_CONFIG0       0x50
-#define ICM42688_GYRO_CONFIG0        0x4F
-#define ICM42688_GYRO_ACCEL_CONFIG0  0x52
-#define ICM42688_INT_CONFIG          0x14
-#define ICM42688_TEMP_DATA1          0x1D
 
-#define ICM42688_GYRO_ODR            0x06
-#define ICM42688_GYRO_RNG            0x00
-#define ICM42688_ACC_ODR             0x06
-#define ICM42688_ACC_RNG             0x02
 
 ICM42688_RawData MPU_Data;
+u32 IIC_Timeout_Cnt = 0;
+u32 IIC_Timeout_Cnt_noTimesClear = 0;
 
 static uint8_t s_device_address = ICM42688_ADDRESS_DEFAULT;
+
+static uint8_t icm42688_normalize_address(uint8_t dev_address)
+{
+    if (dev_address > 0x7FU)
+    {
+        return (uint8_t)(dev_address >> 1);
+    }
+
+    return dev_address;
+}
 
 static ICM42688_Status icm42688_probe_address(uint8_t addr)
 {
@@ -35,13 +32,13 @@ static ICM42688_Status icm42688_probe_address(uint8_t addr)
     return (who_am_i == ICM42688_WHOAMI_VALUE) ? ICM42688_OK : ICM42688_ERROR;
 }
 
-static ICM42688_Status icm42688_write_reg(uint8_t reg_addr, uint8_t data)
+static ICM42688_Status icm42688_write_reg_current(uint8_t reg_addr, uint8_t data)
 {
     I2C1_Status status = i2c1_mem_write(s_device_address, reg_addr, &data, 1U, 20000U);
     return (status == I2C1_OK) ? ICM42688_OK : ((status == I2C1_TIMEOUT) ? ICM42688_TIMEOUT : ICM42688_ERROR);
 }
 
-static ICM42688_Status icm42688_read_reg(uint8_t reg_addr, uint8_t *data)
+static ICM42688_Status icm42688_read_reg_current(uint8_t reg_addr, uint8_t *data)
 {
     I2C1_Status status = i2c1_mem_read(s_device_address, reg_addr, data, 1U, 20000U);
     return (status == I2C1_OK) ? ICM42688_OK : ((status == I2C1_TIMEOUT) ? ICM42688_TIMEOUT : ICM42688_ERROR);
@@ -75,7 +72,44 @@ ICM42688_Status ICM42688_ReadWhoAmI(uint8_t *who_am_i)
         return ICM42688_ERROR;
     }
 
-    return icm42688_read_reg(ICM42688_WHO_AM_I, who_am_i);
+    return icm42688_read_reg_current(ICM42688_WHO_AM_I, who_am_i);
+}
+
+void ICM42688_WriteReg(uint8_t DevAddress, uint8_t RegAddress, uint8_t Data)
+{
+    I2C1_Status status = i2c1_mem_write(icm42688_normalize_address(DevAddress), RegAddress, &Data, 1U, 20000U);
+    if (status == I2C1_TIMEOUT)
+    {
+        IIC_Timeout_Cnt++;
+        IIC_Timeout_Cnt_noTimesClear++;
+    }
+}
+
+uint8_t ICM42688_ReadReg(uint8_t DevAddress, uint8_t RegAddress)
+{
+    uint8_t data = 0;
+    I2C1_Status status = i2c1_mem_read(icm42688_normalize_address(DevAddress), RegAddress, &data, 1U, 20000U);
+    if (status == I2C1_TIMEOUT)
+    {
+        IIC_Timeout_Cnt++;
+        IIC_Timeout_Cnt_noTimesClear++;
+    }
+
+    return data;
+}
+
+int16_t GetData_Gyro(uint8_t REG_Address)
+{
+    uint8_t hd = ICM42688_ReadReg(s_device_address, REG_Address);
+    uint8_t ld = ICM42688_ReadReg(s_device_address, (uint8_t)(REG_Address + 1U));
+    return (int16_t)(((uint16_t)hd << 8) | ld);
+}
+
+int16_t GetData_Acc(uint8_t REG_Address)
+{
+    uint8_t hd = ICM42688_ReadReg(s_device_address, REG_Address);
+    uint8_t ld = ICM42688_ReadReg(s_device_address, (uint8_t)(REG_Address + 1U));
+    return (int16_t)(((uint16_t)hd << 8) | ld);
 }
 
 ICM42688_Status ICM42688_Init(void)
@@ -90,32 +124,32 @@ ICM42688_Status ICM42688_Init(void)
         }
     }
 
-    if (icm42688_write_reg(ICM42688_REG_BANK_SEL, 0x00) != ICM42688_OK)
+    if (icm42688_write_reg_current(ICM42688_REG_BANK_SEL, 0x00) != ICM42688_OK)
     {
         return ICM42688_TIMEOUT;
     }
 
-    if (icm42688_write_reg(ICM42688_PWR_MGMT0, 0x0F) != ICM42688_OK)
+    if (icm42688_write_reg_current(ICM42688_PWR_MGMT0, 0x0F) != ICM42688_OK)
     {
         return ICM42688_TIMEOUT;
     }
 
-    if (icm42688_write_reg(ICM42688_ACCEL_CONFIG0, (uint8_t)((ICM42688_ACC_RNG << 5) | ICM42688_ACC_ODR)) != ICM42688_OK)
+    if (icm42688_write_reg_current(ICM42688_ACCEL_CONFIG0, (uint8_t)((ICM42688_ACC_RNG << 5) | ICM42688_ACC_ODR)) != ICM42688_OK)
     {
         return ICM42688_TIMEOUT;
     }
 
-    if (icm42688_write_reg(ICM42688_GYRO_CONFIG0, (uint8_t)((ICM42688_GYRO_RNG << 5) | ICM42688_GYRO_ODR)) != ICM42688_OK)
+    if (icm42688_write_reg_current(ICM42688_GYRO_CONFIG0, (uint8_t)((ICM42688_GYRO_RNG << 5) | ICM42688_GYRO_ODR)) != ICM42688_OK)
     {
         return ICM42688_TIMEOUT;
     }
 
-    if (icm42688_write_reg(ICM42688_GYRO_ACCEL_CONFIG0, 0x44) != ICM42688_OK)
+    if (icm42688_write_reg_current(ICM42688_GYRO_ACCEL_CONFIG0, 0x44) != ICM42688_OK)
     {
         return ICM42688_TIMEOUT;
     }
 
-    if (icm42688_write_reg(ICM42688_INT_CONFIG, 0x1B) != ICM42688_OK)
+    if (icm42688_write_reg_current(ICM42688_INT_CONFIG, 0x1B) != ICM42688_OK)
     {
         return ICM42688_TIMEOUT;
     }
@@ -131,9 +165,12 @@ void ImuSensor_Init(void)
 void ImuSensor_ReadReg_BuffAll(void)
 {
     uint8_t imu_sensor_buff[14] = {0};
+    IIC_Timeout_Cnt = 0;
 
     if (icm42688_read_regs(ICM42688_TEMP_DATA1, imu_sensor_buff, 14U) != ICM42688_OK)
     {
+        IIC_Timeout_Cnt++;
+        IIC_Timeout_Cnt_noTimesClear++;
         return;
     }
 
@@ -146,8 +183,7 @@ void ImuSensor_ReadReg_BuffAll(void)
     MPU_Data.Temp = (int16_t)((imu_sensor_buff[0] << 8) | imu_sensor_buff[1]);
 }
 
-ICM42688_Status GET_MPU_DATA(void)
+void GET_MPU_DATA(void)
 {
     ImuSensor_ReadReg_BuffAll();
-    return ICM42688_OK;
 }
--- module/FFCY_NEW_ICM42688.h
+++ module/FFCY_NEW_ICM42688.h
@@ -3,6 +3,25 @@
 
 #include "stm32f4xx.h"
 
+#define ICM42688_ADDRESS_DEFAULT      0x68
+#define ICM42688_ADDRESS_ALT          0x69
+#define ICM42688_WHO_AM_I            0x75
+#define ICM42688_WHOAMI_VALUE        0x47
+#define ICM42688_REG_BANK_SEL        0x76
+#define ICM42688_PWR_MGMT0           0x4E
+#define ICM42688_ACCEL_CONFIG0       0x50
+#define ICM42688_GYRO_CONFIG0        0x4F
+#define ICM42688_GYRO_ACCEL_CONFIG0  0x52
+#define ICM42688_INT_CONFIG          0x14
+#define ICM42688_TEMP_DATA1          0x1D
+
+#define ICM42688_GYRO_ODR            0x06
+#define ICM42688_GYRO_RNG            0x00
+#define ICM42688_ACC_ODR             0x06
+#define ICM42688_ACC_RNG             0x02
+
+
+
 typedef enum
 {
     ICM42688_OK = 0,
@@ -21,12 +40,20 @@ typedef struct
     int16_t Temp;
 } ICM42688_RawData;
 
+typedef ICM42688_RawData MPU6050_Data;
+
 extern ICM42688_RawData MPU_Data;
+extern u32 IIC_Timeout_Cnt;
+extern u32 IIC_Timeout_Cnt_noTimesClear;
 
 void ImuSensor_Init(void);
 ICM42688_Status ICM42688_Init(void);
 void ImuSensor_ReadReg_BuffAll(void);
 ICM42688_Status ICM42688_ReadWhoAmI(uint8_t *who_am_i);
-ICM42688_Status GET_MPU_DATA(void);
+void ICM42688_WriteReg(uint8_t DevAddress, uint8_t RegAddress, uint8_t Data);
+uint8_t ICM42688_ReadReg(uint8_t DevAddress, uint8_t RegAddress);
+int16_t GetData_Gyro(uint8_t REG_Address);
+int16_t GetData_Acc(uint8_t REG_Address);
+void GET_MPU_DATA(void);
 
 #endif
